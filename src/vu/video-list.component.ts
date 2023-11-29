a7650c398a70ab15f02540b13b98d5dd08c66f5e
Rajan Kalimuthu Pandian <113327158+rajankpwoolies@users.noreply.github.com>
2023-11-29 11:08:55 +1100

    Created video list component

---
+++ src/vu/video-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'vu-video-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './video-list.component.html',
+  styles: ``
+})
+export class VideoListComponent {
+
+}
